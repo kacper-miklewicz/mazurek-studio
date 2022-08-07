2fb38cbf7cafc546c2c568f653d5971fd81b7a7e
kacper-miklewicz <kacper.miklewicz@gmail.com>
2022-08-07 16:35:26 +0200

    move home and projects to one folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Header from "./components/header/Header";
 import Offer from "./pages/Offer";
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="o-mnie" element={<About />} />
-        <Route path="projekty" element={<Projects />} />
         <Route path="oferta" element={<Offer />} />
         <Route path="kontakt" element={<Contact />} />
         <Route path="admin" element={<Admin />} />
---
+++ src/pages/home/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects: React.FC = () => {
+  return (
+    <main>
+      <h1 className="text-2xl">Projects</h1>
+    </main>
+  );
+};
+
+export default Projects;
