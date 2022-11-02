3926e3cd11335853ce8759b60455ebc4323d248a
kacper-miklewicz <kacper.miklewicz@gmail.com>
2022-11-02 18:59:26 +0100

    set reversed about section alignment

--- src/pages/about/StudioDescription.tsx
+++ src/pages/about/StudioDescription.tsx
@@ -1,16 +1,17 @@
+import AboutSection from "./AboutSection";
+
 interface StudioDescriptionProps {}
 
 const StudioDescription: React.FC<StudioDescriptionProps> = () => {
   return (
-    <section className="studio-description flex flex-col gap-6 pb-20 mb-20 xs:gap-12">
-      <h2 className="about-title">Mazurek Studio</h2>
-      <picture className="mx-auto">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-studio-mobile.png?alt=media&token=0bf163f8-c119-4da6-a482-00848c71e78c"
-          alt=""
-        />
-      </picture>
-      <p className="about-text">
+    <AboutSection
+      title="Mazurek Studio"
+      reversed={true}
+      mobileImg="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-studio-mobile.png?alt=media&token=0bf163f8-c119-4da6-a482-00848c71e78c"
+      desktopImg="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-studio.png?alt=media&token=8d410b02-707f-4932-a184-03fd590e167b"
+      className="md:pb-56 md:mb-56"
+    >
+      <p className="about-text md:mb-10">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
         nesciunt! Consequuntur quam numquam iste quibusdam beatae autem optio
         vitae sapiente, magni voluptatum culpa voluptas, facere mollitia alias
@@ -24,7 +25,7 @@ const StudioDescription: React.FC<StudioDescriptionProps> = () => {
         voluptatum exercitationem fugit quam illum quaerat, adipisci odit
         suscipit voluptatem quia? Debitis, ratione!
       </p>
-    </section>
+    </AboutSection>
   );
 };
 
--- src/pages/about/StudioOwner.tsx
+++ src/pages/about/StudioOwner.tsx
@@ -1,15 +1,15 @@
+import AboutSection from "./AboutSection";
+
 interface StudioOwnerProps {}
 
 const StudioOwner: React.FC<StudioOwnerProps> = () => {
   return (
-    <section className="flex flex-col gap-6 xs:gap-12">
-      <h2 className="about-title">Karolina Mazurek</h2>
-      <picture>
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-owner-mobile.png?alt=media&token=5acd938c-fe2b-4eff-b7a2-d2beab1017eb"
-          alt=""
-        />
-      </picture>
+    <AboutSection
+      title="Karolina Mazurek"
+      reversed={false}
+      mobileImg="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-owner-mobile.png?alt=media&token=5acd938c-fe2b-4eff-b7a2-d2beab1017eb"
+      desktopImg="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-owner.png?alt=media&token=325c95de-14f4-48f9-bdfd-0366912e3bd3"
+    >
       <p className="about-text">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
         vitae distinctio pariatur modi minus odit saepe atque necessitatibus,
@@ -18,7 +18,7 @@ const StudioOwner: React.FC<StudioOwnerProps> = () => {
         Cupiditate maiores alias voluptatibus sunt placeat mollitia temporibus
         saepe nihil nisi minus. Ea, vel!
       </p>
-    </section>
+    </AboutSection>
   );
 };
 
