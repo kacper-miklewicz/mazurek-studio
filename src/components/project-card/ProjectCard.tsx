78d5c741b58e886638d61988f647617b556b28c1
kacper-miklewicz <kacper.miklewicz@gmail.com>
2022-11-20 13:38:15 +0100

    fixed styling issues

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -9,9 +9,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
   return (
     <li className="overflow-hidden project-card">
       <Link to={`projekty/${data.projectId}`}>
-        <h3 className="text-xl mb-2 md:block md:text-3xl">{data.title}</h3>
+        <h3 className="hidden text-xl mb-2 md:block md:text-2xl">
+          {data.title}
+        </h3>
         <div
-          className="h-[500px] bg-cover bg-center relative transition-all duration-300 project-card-img l:h-[600px] xl:h-[700px]"
+          className="h-[500px] bg-cover bg-center relative transition-all duration-300 project-card-img l:h-[550px] xl:h-[600px]"
           style={{ backgroundImage: `url(${data.coverPhotoURL}}` }}
         ></div>
       </Link>
