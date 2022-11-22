a802f736d20b2376fa307f0448cb65f6696832d8
kacper-miklewicz <kacper.miklewicz@gmail.com>
2022-11-22 19:31:27 +0100

    changed about section

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,10 +1,42 @@
-import StudioDescription from "./StudioDescription";
-import StudioOwner from "./StudioOwner";
+import { Link } from "react-router-dom";
+import {
+  paragraphs as textParagraphs,
+  offerParagraph,
+  finalParagraph,
+} from "./consts/textParagraphs";
 
 const About: React.FC = () => {
   return (
     <main className="px-5 py-10 mx-auto max-w-[550px] xs:px-16 md:max-w-[1480px]">
-      <StudioOwner />
+      <h2 className="section-title md:mb-10">O mnie</h2>
+      <section className="about-section flex flex-col gap-6 xs:gap-12 md:items-center md:flex-row md:mb-20">
+        <div className="md:w-1/2 md:flex-grow md:mr-10 l:mr-10">
+          <picture className="mx-auto">
+            <source
+              media="(min-width: 1120px)"
+              srcSet="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-owner.png?alt=media&token=325c95de-14f4-48f9-bdfd-0366912e3bd3"
+            />
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fo-mnie%2Fo-mnie-owner-mobile.png?alt=media&token=5acd938c-fe2b-4eff-b7a2-d2beab1017eb"
+              alt="about section"
+            />
+          </picture>
+        </div>
+        <div className="md:w-1/2">
+          {textParagraphs.map(paragraph => (
+            <p key={paragraph} className="section-text md:mb-6">
+              {paragraph}
+            </p>
+          ))}
+          <Link
+            to="/oferta"
+            className="section-text block underline hover:text-black/80"
+          >
+            {offerParagraph}
+          </Link>
+          <p className="section-text">{finalParagraph}</p>
+        </div>
+      </section>
     </main>
   );
 };
--- src/pages/about/consts/textParagraphs.ts
+++ src/pages/about/consts/textParagraphs.ts
@@ -1,9 +1,11 @@
-const paragraphs = [
+export const paragraphs = [
   "Cześć, tu Karolina Mazurek!",
   "Jeśli marzysz o wnętrzu szytym na miarę to świetnie trafiłeś!",
   "Zajmuję się kompleksowym projektowaniem wnętrz, od koncepcji po nadzór autorski. Zanim jednak przejdę do tworzenia układów funkcjonalnych, wizualizacji czy rysunków technicznych chcę lepiej poznać klienta, jego zainteresowania, styl życia, po to by stworzyć miejsce, w którym będzie czuł się dobrze.",
   "Zapraszam do zapoznania się z ofertą.",
-  "Stwórzmy razem Twoje wymarzone miejsce na ziemi!",
 ];
 
-export default paragraphs;
+export const offerParagraph = "Kliknij tutaj, aby przejść do oferty.";
+
+export const finalParagraph =
+  "Stwórzmy razem Twoje wymarzone miejsce na ziemi!";
