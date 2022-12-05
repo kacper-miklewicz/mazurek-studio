18fb9920727e0573a92a9b192353fd2e1f6b02ed
kacper-miklewicz <kacper.miklewicz@gmail.com>
2022-12-05 10:47:55 +0100

    changed photo size in contact section

--- src/pages/contact/ContactPhotos.tsx
+++ src/pages/contact/ContactPhotos.tsx
@@ -2,7 +2,7 @@ interface ContactPhotosProps {}
 
 const ContactPhotos: React.FC<ContactPhotosProps> = () => {
   return (
-    <picture className="grow max-w-[550px]">
+    <picture className="grow max-w-[450px] l:max-w-[550px]">
       <img
         src="https://firebasestorage.googleapis.com/v0/b/mazurek-studio.appspot.com/o/static%2Fimg%2Fkontakt%2Fkontakt.jpg?alt=media&token=fe4a57c5-48a4-4b33-8ddc-10b06e954ca7"
         alt="Karolina Mazurek"
